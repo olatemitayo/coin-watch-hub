2f6ef8d774be663a4ddecf1d0ec4c4d8d8ab0ad0
olatemitayo <olatunjitemitayo444@gmail.com>
2024-01-14 09:18:07 +0100

    few changes

--- components/trending.tsx
+++ components/trending.tsx
@@ -1,27 +1,31 @@
-import { builder } from '@/api/builder'
-import { useCustomTable } from '@/hooks/custom-data'
-import { Stack } from '@mantine/core'
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-import { DataTable } from './custom-table'
-import { TrendingTableColumns } from './trending-table-column'
+import { builder } from "@/api/builder";
+import { useCustomTable } from "@/hooks/custom-data";
+import { Stack } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { DataTable } from "./custom-table";
+import { TrendingTableColumns } from "./trending-table-column";
+import { ProductLoading } from ".";
 
-export  function Trending() {
-     // get trending list 
- const {data: trendingData, isLoading, isError} = useQuery({
+export function Trending() {
+  // get trending list
+  const {
+    data: trendingData,
+    isLoading,
+    isError,
+  } = useQuery({
     queryFn: () => builder.use().asset.trending(),
     queryKey: builder.asset.trending.get(),
-    select: ({data}) => data?.coins?.map((item) => item?.item)
-   })
-   console.log({trendingData})
-   const { table } = useCustomTable({
+    select: ({ data }) => data?.coins?.map((item) => item?.item),
+  });
+  console.log({ trendingData });
+  const { table } = useCustomTable({
     tableData: trendingData,
     columns: TrendingTableColumns,
   });
   return (
-    <Stack  px={50}>
-
-<DataTable table={table}/>
+    <Stack px={50}>
+      {isLoading ? <ProductLoading /> : <DataTable table={table} />}
     </Stack>
-  )
+  );
 }
